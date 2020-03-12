be619d5aa0b4ebb7c7628731977159c24f4fbe4f
mariamisa <maryaam1998@hotmail.com>
2020-03-12 22:29:37 +0200

    auth

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -9,11 +9,11 @@ if (process.env.NODE_ENV === 'test') {
 } else {
   dbUrl = process.env.DB_URL;
 }
-if (!dbUrl) throw new Error('No Database URL!!!');
+// if (!dbUrl) throw new Error('No Database URL!!!');
 
 const options = {
   connectionString: dbUrl,
-  ssl: false,
+  ssl: true,
 };
 
 module.exports = new Pool(options);
