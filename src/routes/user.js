7d60d0f904ce83293af69269ff907faff9106575
mariamisa <maryaam1998@hotmail.com>
2020-03-13 12:38:02 +0200

    fix routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,10 +6,10 @@ const verifyPassword=require('../middleware/verifyUser');
 const {protected,loginPro}=require('../middleware/protectRoute');
 const authPage=require('../controller/authPage');
 
-Router.post('/create-user',signValidation,emailNotExist,createUser)
-Router.get('/auth',loginPro,authPage)
-Router.post('/login',loginValidation,emailExist,verifyPassword,login)
-Router.get('/posts',protected,posts)
-Router.post('/logout',logout)
+Router.post('/create-user',signValidation,emailNotExist,createUser) //signup
+Router.post('/login',loginValidation,emailExist,verifyPassword,login) //login
+Router.get('/auth',loginPro,authPage) //login or signup page
+Router.get('/posts',protected,posts) //posts page
+Router.get('/logout',logout) //logout
 
 module.exports=Router
